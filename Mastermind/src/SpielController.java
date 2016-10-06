e4f035e85a8c3cefb8bfb036263666e6eea9cc8f
Frank Eberhardt <frank@feberhardt.de>
2016-10-06 13:13:44 +0200

    alles gut

---
+++ Mastermind/src/SpielController.java
@@ -0,0 +1,21 @@
+
+public class SpielController {
+	
+	/*Controller verknüpft View mit Spiel (Modell) Sinn: 
+	Spiel (Modell) wird vom View getrennt!! 
+	Separierung der Zuständigkeiten.
+	*/
+	private Spiel spiel;
+	private View view;
+	
+	//Zahlen vom View anfordern
+	
+	
+	//Zahlen vom View an Spiel übergeben
+	
+	
+	//Ergebnis wieder an View übergeben
+	
+	
+
+}
