2451d7d6826b0c7a33fe655a56d06091ec281699
frank <frank@franksmbp.fritz.box>
2016-10-06 13:08:36 +0200

    neu

---
+++ Mastermind/src/MastermindPrg.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MastermindPrg {
+	
+	int maximaleParalleleSpiele=1;
+	private List<Spiel> alleSpiele=new ArrayList<Spiel>();
+	
+	
+	private KonsolenView[] k;
+
+	public static void main(String[] args) {
+		//neues Spiel
+		//s[1]=new Spiel(anzahlFarben);
+		// TODO Auto-generated method stub
+	}
+}
---
+++ Mastermind/src/View.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+/**
+ * 
+ */
+
+/**
+ * @author frank
+ *
+ */
+public interface View {
+
+	List<Integer> zug(int felder);
+	
+	//hier die View Methoden: neuerTipp(); neuesSpiel(parameter);
+
+}
